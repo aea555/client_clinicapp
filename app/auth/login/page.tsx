ea1da6c6d1a3fc87e1a921abf6d966ff0ff5741f
aea555 <akarahmet2002@gmail.com>
2024-09-14 00:53:10 +0300

    .

--- app/auth/login/page.tsx
+++ app/auth/login/page.tsx
@@ -6,7 +6,7 @@ export default function LoginPage() {
   return (
     <div className="container flex h-screen w-screen flex-col items-center justify-center lg:max-w-none lg:grid-cols-2 lg:px-0">
       <div className="flex flex-col items-center justify-center p-4 lg:p-8 w-full">
-        <div className="flex flex-col space-y-6 w-1/3 max-w-sm sm:max-w-md md:max-w-lg lg:max-w-xl xl:max-w-2xl">
+        <div className="flex flex-col space-y-6 max-w-sm sm:max-w-md md:max-w-lg lg:max-w-xl xl:max-w-2xl">
           <Suspense fallback={<div className='text-center'><Spinner size="xl" aria-label="Loading" /></div>}>
             <LoginForm />
           </Suspense>

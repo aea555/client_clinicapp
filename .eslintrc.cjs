d0005bc02a8a8a48effc952e5e400c44cb462e4a
aea555 <akarahmet2002@gmail.com>
2024-09-14 00:29:14 +0300

    .

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -30,7 +30,7 @@ const config = {
     ],
     "@typescript-eslint/require-await": "off",
     "@typescript-eslint/no-misused-promises": [
-      "error",
+      "warn",
       {
         "checksVoidReturn": {
           "attributes": false
